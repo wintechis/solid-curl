926c779975d837705ae86d6ea4b3e97b53ea4f68
Daniel Schraudner <daniel.schraudner@fau.de>
2021-12-10 16:12:16 +0100

    Prompting for credentials

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const express = require("express");
-const { 
-  getSessionFromStorage,
-  getSessionIdFromStorageAll,
-  Session
-} = require("@inrupt/solid-client-authn-node");
-const open = require('open');
-//const fetch = require('node-fetch');
-//const superagent = require('superagent');
+const { Session } = require("@inrupt/solid-client-authn-node");
 const puppeteer = require('puppeteer');
-const https = require('https');
-//const { JSDOM } = require('jsdom');
-//const { createHttpTerminator } = require('http-terminator');
 const process = require('process');
+const readlineSync = require('readline-sync');
 
+// Remove draft warning from oidc-client lib
 process.emitWarning = (warning, ...args) => {
 	if (args[0] === 'DraftWarning') {
 		return;
 	}
-
 	return emitWarning(warning, ...args);
 };
 
 const session = new Session();
-const app = express();
 
-const httpAgent = new https.Agent({
-	keepAlive: true
+// Try request without authentication
+session.fetch(process.argv[2], {
+	method: 'HEAD',
+}).then(async res => {
+	if(res.status !== 401) {
+		// No 401 Unauthenticated so do real request and exit
+		session.fetch(process.argv[2]).then(async res => {
+			let text = await res.text();
+			console.log(text);
+			process.exit();
+		});
+	}
 });
+// Else authenticate
 
-let prom = null;
+const app = express();
 let server = null;
 
+// Handler for the redirect from the IdP
 app.get("/", async (req, res, next) => {
 	await session.handleIncomingRedirect(`http://localhost:8988${req.url}`);
 	res.sendStatus(200);
+
+	// Do actual request
 	session.fetch(process.argv[2]).then(async res => {
 		let text = await res.text();
 		console.log(text);
@@ -42,18 +46,45 @@ app.get("/", async (req, res, next) => {
 });
 server = app.listen(8988);
 
+// Log in
+let oidcIssuer = readlineSync.question(`Solid OIDC Provider URI: `);
 session.login({
 	redirectUrl: 'http://localhost:8988/',
-	oidcIssuer: 'https://solid.dschraudner.de/',
+	oidcIssuer: oidcIssuer,
 	handleRedirect: handleRedirect,
 })
 
+// Redirect Handler: Fill out the login form
 async function handleRedirect(url) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(url);
-	await page.type('#email', 'myemail');
-	await page.type('#password', 'mypassword');
-	await page.click('button[type=submit]');
+	let emailField = await page.$('#email');
+	if(emailField) {
+		let emailLabel = await page.$eval('label[for=email]', el => el.innerHTML);
+		let email = readlineSync.question(`${emailLabel}: `);
+		await emailField.type(email);
+	}
+
+	let passwordField = await page.$('#password');
+	if(passwordField) {
+		let passwordLabel = await page.$eval('label[for=password]', el => el.innerHTML);
+		let password = readlineSync.question(`${passwordLabel}: `, {
+			hideEchoBack: true,
+		});
+		await passwordField.type(password);
+	}
+	process.stdout.write('\n');
+
+	let res = Promise.race([
+		page.waitForResponse(),
+		new Promise((resolve) => setTimeout(() => resolve(null), 1000)),
+	]);
+	page.click('button[type=submit]');
+
+	if(await res === null) {
+		console.error('Authentication did not succeed!');
+		process.exit(1);
+	}
 	await browser.close();
 }
