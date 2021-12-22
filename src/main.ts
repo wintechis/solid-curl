0747aef738518546981ae55891df24b32062a104
Daniel Schraudner <daniel.schraudner@fau.de>
2021-12-22 13:08:23 +0100

    Added verbose logging

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ program
 	.option('-f, --fail', 'Fail silently (no output at all) on HTTP errors')
 	.option('-H, --header <header...>', 'Add header to request')
 	.option('-i, --include', 'Include HTTP response headers in output')
+	.option('-L, --location', 'Follow redirects')
 	.option('-o, --output <file>', 'Write to file instead of stdout')
 	.option('-O, --remote-name', 'Write output to a file named as the remote file')
 	.option('-s, --silent', 'Silent mode')
@@ -35,10 +36,21 @@ program
 program.parseAsync();
 
 async function run(uri: string, options: any) {
+	if(options.verbose) {
+		logger.setLevel('info');
+	}
+
 	const session = new Session();
-	let headers: Record<string,string> = {};
+	let headers: Record<string,string> = {
+		'accept': 'text/turtle, */*;q=0.8',
+		'user-agent': 'solid-curl/0.1.0',
+		'accept-encoding': 'gzip,deflate',
+		'connection': 'close',
+		'host': uri.split('/').slice(2,3).join()
+	};
 	let fetchInit: RequestInit = {
-		method: options.method
+		method: options.request,
+		redirect: options.location ? 'follow' : 'manual'
 	};
 
 	// Loading data from file if necessary
@@ -62,8 +74,19 @@ async function run(uri: string, options: any) {
 	// Do unauthenticated request when no user is provided
 	if(!user) {
 		logger.info('* No user identity given, doing unauthenticated request');
-		const res = await session.fetch(uri, fetchInit);
-		const text = await res.text();
+		logger.info(`> ${fetchInit.method} /${uri.split('/').slice(3).join('/')} HTTP/1.1`);
+		for(let h in headers) {
+			// Make header names upercase for logging
+			logger.info(`> ${h.split('-').map((s: string) => s.charAt(0).toUpperCase() + s.slice(1)).join('-')}: ${headers[h]}`);
+		}
+		logger.info('>');
+		let res = await session.fetch(uri, fetchInit);
+		logger.info(`< HTTP/1.1 ${res.status} ${res.statusText}`);
+		for(let h of res.headers) {
+			logger.info(`< ${h[0].split('-').map((s: string) => s.charAt(0).toUpperCase() + s.slice(1)).join('-')}: ${h[1]}`);
+		}
+		logger.info('<');
+		let text = await res.text();
 		console.log(text);
 		process.exit();
 	}
@@ -72,31 +95,45 @@ async function run(uri: string, options: any) {
 	let server = null;
 
 	// Handler for the redirect from the IdP
-	app.get("/", async (req, res) => {
+	app.get("/", async (req, expressRes) => {
+		logger.info(`* Received redirect from IdP to: ${req.url}`);
 		await session.handleIncomingRedirect(`http://localhost:29884${req.url}`);
-		res.sendStatus(200);
+		expressRes.sendStatus(200);
 
+		logger.info(`* Doing actual request authenticated as ${session.info.webId}`);
 		// Do actual request
-		session.fetch(uri, fetchInit).then(async res => {
-			let text = await res.text();
-			console.log(text);
-			process.exit();
-		});
+		logger.info(`> ${fetchInit.method} /${uri.split('/').slice(3).join('/')} HTTP/1.1`);
+		for(let h in headers) {
+			// Make header names upercase for logging
+			logger.info(`> ${h.split('-').map((s: string) => s.charAt(0).toUpperCase() + s.slice(1)).join('-')}: ${headers[h]}`);
+		}
+		logger.info('>');
+		let res = await session.fetch(uri, fetchInit);
+		logger.info(`< HTTP/1.1 ${res.status} ${res.statusText}`);
+		for(let h of res.headers) {
+			logger.info(`< ${h[0].split('-').map((s: string) => s.charAt(0).toUpperCase() + s.slice(1)).join('-')}: ${h[1]}`);
+		}
+		logger.info('<');
+		let text = await res.text();
+		console.log(text);
+		process.exit();
 	});
 	server = app.listen(29884);
 
 	// Try to load credentials from config
-	const config = require('./.solid-curl-ids.json');
+	const config = require('../.solid-curl-ids.json');
 	const {
 		oidcProvider: configOidcProvider,
 		email: configEmail,
 		username: configUsername,
 		password: configPassword,
 	} = config[options.user];
+	logger.info(`* Loaded credentials of identity ${options.user} from config file`);
 
 	// Log in
 	let oidcIssuer = configOidcProvider// ? configOidcProvider : readlineSync.question(`Solid OIDC Provider URI: `);
-	session.login({
+	logger.info(`* Initiating OIDC login at ${oidcIssuer}`);
+	await session.login({
 		redirectUrl: 'http://localhost:29884/',
 		oidcIssuer: oidcIssuer,
 		handleRedirect: handleRedirect,
@@ -109,17 +146,18 @@ async function run(uri: string, options: any) {
 	}
 	*/
 
-
 	// Redirect Handler: Fill out the login form
 	async function handleRedirect(url: string) {
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
+		logger.info(`* Fetching login page provided by IdP: ${url}`);
 		await page.goto(url);
 
 		let emailField = await page.$('#email');
 		if(emailField) {
 			let emailLabel = await page.$eval('label[for=email]', el => el.innerHTML);
 			let email = configEmail// ? configEmail : readlineSync.question(`${emailLabel}: `);
+			logger.info(`* Entering email to form: ${email}`);
 			await emailField.type(email);
 		}
 
@@ -127,6 +165,7 @@ async function run(uri: string, options: any) {
 		if(usernameField) {
 			let usernameLabel = await page.$eval('label[for=username]', el => el.innerHTML);
 			let username = configUsername// ? configUsername : readlineSync.question(`${usernameLabel}: `);
+			logger.info(`* Entering username to form: ${username}`);
 			await usernameField.type(username);
 		}
 
@@ -136,6 +175,7 @@ async function run(uri: string, options: any) {
 			let password = configPassword// ? configPassword : readlineSync.question(`${passwordLabel}: `, {
 			//	hideEchoBack: true,
 			//});
+			logger.info(`* Entering password to form: ${password.replaceAll(new RegExp('.', 'g'), '*')}`);
 			await passwordField.type(password);
 		}
 
@@ -143,18 +183,17 @@ async function run(uri: string, options: any) {
 			page.waitForNavigation(),
 			new Promise((resolve) => setTimeout(() => resolve(null), 3000)),
 		]);
+		logger.info(`* Submitting login form`);
 		await page.click('button[type=submit]');
 		let res = await resP;
 
 		if(res === null) {
-			await page.screenshot({ path: 'example.png' });
-			logger.error('Authentication did not succeed!');
+			logger.error('Authentication did not succeed: Timed out!');
 			process.exit(1);
 		} else {
 			// node-solid-server may redirect to another form that needs a submit
 			if((res as HTTPResponse).status() !== 200) {
-				await page.screenshot({ path: 'example.png' });
-				logger.error('Authentication did not succeed!');
+				logger.error(`Authentication did not succeed: ${(res as HTTPResponse).status()} ${(res as HTTPResponse).statusText()}`);
 				process.exit(2);
 			} else {
 				let submit = await page.$('button[type=submit]')
